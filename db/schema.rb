791f142074acfc309f436fa68561a09f8c997811
Omar Ozgur <oozgur217@gmail.com>
2016-10-07 15:32:22 -0700

    Added posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930221907) do
+ActiveRecord::Schema.define(version: 20161007222637) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "description"
+    t.text     "snippit"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
