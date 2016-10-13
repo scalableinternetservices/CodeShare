1e3f51007db9872df0c7f4c0c5d3623055ba59f9
shannonphu <shannonphu@sbcglobal.net>
2016-10-13 00:56:09 -0700

    add first+last name to user model and get signin+edit user working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007223334) do
+ActiveRecord::Schema.define(version: 20161013073107) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20161007223334) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
