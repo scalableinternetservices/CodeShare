55724d2d7d91ee4447f1d3dda0ca0439eb013997
shannonphu <shannonphu@sbcglobal.net>
2016-10-14 09:44:55 -0700

    set default avatar to name or email depending on presence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,17 @@ class UsersController < ApplicationController
 	def show
 	    if user_signed_in?
 	      @user = current_user
-	      @avatar_url = Dragonfly.app.generate(:initial_avatar, @user.email, {background_color: '00695c', size: '200'}).url
+
+	      # If user set their name, use that for the default avatar. Otherwise default to the email.
+	      avatar_name = @user.email
+	      if @user.first_name
+	      	avatar_name = @user.first_name
+	      end
+	      if @user.last_name
+	      	avatar_name << " " << @user.last_name
+      	  end
+
+	      @avatar_url = Dragonfly.app.generate(:initial_avatar, avatar_name, {background_color: '00695c', size: '200'}).url
 	    else
 	      redirect_to login_path
 	    end
