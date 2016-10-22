81f8e9d4c8a828386297a21bda903f7c7f57bc82
Michael Wang <micwa6@yahoo.com>
2016-10-22 14:29:48 -0700

    Create tag model and join model for posts/tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013073107) do
+ActiveRecord::Schema.define(version: 20161022205810) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20161013073107) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "post_tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string   "description"
     t.text     "snippit"
@@ -29,6 +38,19 @@ ActiveRecord::Schema.define(version: 20161013073107) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id",  null: false
+    t.index ["post_id"], name: "index_posts_tags_on_post_id"
+    t.index ["tag_id"], name: "index_posts_tags_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
