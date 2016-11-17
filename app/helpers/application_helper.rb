ef4791d26bd4b18f5dae7eb6209e13b2f705ed1b
shannonphu <shannonphu@sbcglobal.net>
2016-11-17 01:03:33 -0800

    update UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,4 +53,19 @@ module ApplicationHelper
 		end
 		return tags
 	end
+
+	def make_avatar_url(user, size=150)
+		    # If user set their name, use that for the default avatar. Otherwise default to the email.
+		    avatar_name = user.email
+
+		    if user.first_name != ''
+				avatar_name = String.new(user.first_name)
+		    end
+		    if user.last_name != ''
+				avatar_name << " " << user.last_name
+			end
+
+		    avatar_url = Dragonfly.app.generate(:initial_avatar, avatar_name, {background_color: "%06x" % (rand * 0xffffff), size: String(size)}).url
+		    return avatar_url
+	end
 end
