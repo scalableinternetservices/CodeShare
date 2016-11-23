9aa2d27eff23839d6c1d7372a1fdf8888932ed49
Michael Wang <micwa6@yahoo.com>
2016-11-22 19:35:20 -0800

    Fix formatting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
-	def index
-		if user_signed_in?
-			@user = current_user
-		else
-			redirect_to login_path
-		end
-	end
+  def index
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-	def show
-		if(params[:id].present?)
-            @user = User.find(params[:id])
-        elsif user_signed_in?
-            @user = current_user
-        else
-			redirect_to login_path
-        end
+  def show
+    if(params[:id].present?)
+      @user = User.find(params[:id])
+    elsif user_signed_in?
+      @user = current_user
+    else
+      redirect_to login_path
+    end
 
-        # If user set their name, use that for the default avatar. Otherwise default to the email.
-        avatar_name = @user.email
+    # If user set their name, use that for the default avatar. Otherwise default to the email.
+    avatar_name = @user.email
 
-        if @user.first_name != ''
-    		avatar_name = String.new(@user.first_name)
-        end
-        if @user.last_name != ''
-    		avatar_name << " " << @user.last_name
-    	end
+    if @user.first_name != ''
+      avatar_name = String.new(@user.first_name)
+    end
+    if @user.last_name != ''
+      avatar_name << " " << @user.last_name
+    end
 
-        @avatar_url = Dragonfly.app.generate(:initial_avatar, avatar_name, {background_color: '00695c', size: '200'}).url
-	end
+    @avatar_url = Dragonfly.app.generate(:initial_avatar, avatar_name, {background_color: '00695c', size: '200'}).url
+  end
 end
