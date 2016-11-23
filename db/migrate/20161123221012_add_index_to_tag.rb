24e99ab7ed58a214572771eae83b821d8504836b
Michael Wang <micwa6@yahoo.com>
2016-11-23 14:13:25 -0800

    Add index on tags.name

---
+++ db/migrate/20161123221012_add_index_to_tag.rb
@@ -0,0 +1,5 @@
+class AddIndexToTag < ActiveRecord::Migration[5.0]
+  def change
+    add_index :tags, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123042540) do
+ActiveRecord::Schema.define(version: 20161123221012) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20161123042540) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name"
   end
 
   create_table "users", force: :cascade do |t|
